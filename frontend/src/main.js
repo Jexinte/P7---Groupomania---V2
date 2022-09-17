c9800b4f374d08e3d5ba820fed8087204e4518f6
Yokke <mdembelepro@gmail.com>
2022-09-17 17:34:13 +0200

    Suite rectification

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,4 +3,14 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import '@/assets/css/main.css'
-createApp(App).use(store).use(router).mount('#app')
+
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/* import specific icons */
+import { faArrowLeft } from '@fortawesome/free-solid-svg-icons'
+library.add(faArrowLeft)
+createApp(App).use(store).use(router).component('font-awesome-icon',FontAwesomeIcon).mount('#app')
